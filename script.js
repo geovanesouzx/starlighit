594c4e703084263bf95d63ef65e49ea0c6266bee
geovanesouzx <sjeje702@gmail.com>
2025-11-14 23:25:35 -0300

    Sua mensagem aqui

--- script.js
+++ script.js
@@ -147,8 +147,10 @@ document.addEventListener('DOMContentLoaded', function () {
         'https://pbs.twimg.com/media/FMs8_KeWYAAtoS3.jpg',
         'https://pbs.twimg.com/media/EcGdw6xXsAANIu1?format=jpg&name=large',
         'https://pbs.twimg.com/media/EcGdw6wXYAUrPu4.jpg',
-        'https://pbs.twimg.com/media/EcGdw6uXgAEpGA-.jpg'
-
+        'https://pbs.twimg.com/media/EcGdw6uXgAEpGA-.jpg',
+        'https://pbs.twimg.com/media/FRnvb21XEAIHLL3?format=jpg&name=4096x4096',
+        'https://pbs.twimg.com/media/FRnvdVzWUAAZweZ?format=jpg&name=4096x4096',
+    ];
     ];
 
     // Ícones SVG para os controles do player
