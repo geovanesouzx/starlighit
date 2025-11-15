096c9c020381ef64336d5ba828b6197d72f1a990
geovanesouzx <sjeje702@gmail.com>
2025-11-14 23:26:36 -0300

    Sua mensagem aqui

--- script.js
+++ script.js
@@ -149,8 +149,7 @@ document.addEventListener('DOMContentLoaded', function () {
         'https://pbs.twimg.com/media/EcGdw6wXYAUrPu4.jpg',
         'https://pbs.twimg.com/media/EcGdw6uXgAEpGA-.jpg',
         'https://pbs.twimg.com/media/FRnvb21XEAIHLL3?format=jpg&name=4096x4096',
-        'https://pbs.twimg.com/media/FRnvdVzWUAAZweZ?format=jpg&name=4096x4096',
-    ];
+        
     ];
 
     // Ícones SVG para os controles do player
